c630430adeea2968b9e182689a041b95d3129457
KolinMTG <colin.manyri@gmail.com>
2024-05-17 09:09:31 +0200

    ajoute de fonctions est_legal et coup_legaux

--- Gopher/new_gopher.py
+++ Gopher/new_gopher.py
@@ -3,7 +3,10 @@ from typing import Union
 import pprint 
 import tkinter as tk
 # Types de base utilisés par l'arbitre
-Grid = np.ndarray
+Grid_pos = np.ndarray # Grille de jeu (tableau 2D de cases)
+Grid_value = np.ndarray
+Grids = tuple[Grid_pos, Grid_value]
+GameValue = int # Valeur d'une case (0, 1 ou 2)
 # Environment = ... # Ensemble des données utiles (cache, état de jeu...) pour
 #               # que votre IA puisse jouer (objet, dictionnaire, autre...)
 Cell = tuple[int, int]
@@ -15,32 +18,34 @@ State = list[tuple[Cell, Player]] # État du jeu pour la boucle de jeu
 Score = int
 Time = int
 
+EMPTY = 0
 
-def init_grille(taille_grille: int) -> Grid:
+def init_grille(taille_grille: int) -> Grids:
     """Initialise la grille de jeu avec des cases vides."""
 
     taille_array = 2*taille_grille+1
-    grille = np.full((taille_array, taille_array), None)
+    grilles = (np.full((taille_array, taille_array), None), np.full((taille_array, taille_array), EMPTY))
     compteur = taille_grille
     for i in range(taille_grille): #remplir la premiere partie
         for j in range(compteur, taille_array):
-            grille[i][j] = (j-taille_grille, taille_grille-i)
+            grilles[0][i][j] = (j-taille_grille, taille_grille-i)
         compteur -=1
     for j in range(taille_array): #remplir la ligne du milieu
-        grille[taille_grille][j] = (j-taille_grille, 0)
+        grilles[0][taille_grille][j] = (j-taille_grille, 0)
     compteur = 1
     for i in range(taille_grille+1, taille_array): #remplir le reste
         for j in range(taille_array-compteur):
-            grille[i][j] = (j-taille_grille, taille_grille-i)
+            grilles[0][i][j] = (j-taille_grille, taille_grille-i)
+            
         compteur +=1
-    return grille
+    return grilles
 
 #!test
-#print(init_grille(7))
+print(init_grille(7))
 
-def voisins(grille:Grid, pos:Cell) -> list[Cell]:
+def voisins(grille:Grid_pos, pos:Cell) -> list[Cell]:
     """renvoie la liste des voisins d'une case donnée"""
-    taille_max_array = np.shape(grille)[0] //2-1
+    taille_max_array = np.shape(grille[0])[0] //2-1
     x, y = pos
     liste_absolue = [(x, y-1), (x, y+1), (x+1, y), (x-1, y), (x+1, y+1), (x-1, y-1)]
     liste_voisins =[]
@@ -54,10 +59,63 @@ def voisins(grille:Grid, pos:Cell) -> list[Cell]:
 # print(voisins(init_grille(7), (0,0)))
 #print(voisins(init_grille(7), (3, -3)))
 
+def get_value(grille:Grid_value, pos:Cell) -> GameValue:
+    """renvoie la valeur d'une case donnée de position tuple pos"""
+    taille_max_array = np.shape(grille)[0] //2-1
+    x, y = pos
+    x += taille_max_array
+    y += taille_max_array
+    print(x, y)
+    return grille[x, y]
+
+#!test
+# print(get_value(init_grille(7)[1], (-6,0)))
+# print(get_value(init_grille(7)[1], (0,0)))
+# print(get_value(init_grille(7)[1], (6,6)))
+
+def put_value(grille:Grid_value, pos:Cell, value:GameValue) -> Grid_value:
+    """modifie la valeur d'une case donnée de position tuple pos"""
+    taille_max_array = np.shape(grille)[0] //2-1
+    x, y = pos
+    x += taille_max_array
+    y += taille_max_array
+    grille[x, y] = value
+    return grille
 
 
-def est_legal(grille:Grid, coup:ActionGopher):
+def est_legal(grille:Grids, coup:ActionGopher, joueur : Player) -> bool:
     """Renvoie True si le coup est légal pour une grille donnée et False sinon"""
+    grid_pos = grille[0]
+    grid_value = grille[1]
+    x, y = coup
+    if grid_value[x, y] != EMPTY: #si la case est deja prise
+        return False
+
+    liste_voisins = voisins(grid_pos, coup)
+    joueur_1 = 0
+    joueur_2 = 0
+    for voisin in liste_voisins:
+        if get_value(grid_value, voisin) == joueur: #si la case est en contacte avec une case du même joueur 
+            joueur_1 +=1
+        elif grid_value[voisin] != EMPTY: #si la case est en contacte avec une case du joueur adverse
+            joueur_2 +=1
+    if joueur_1 == 0 and joueur_2 == 1: #exactement 0 cases du joueur et 1 case du joueur adverse
+        return True
+    
+def liste_coup_legaux(grille:Grids, joueur:Player) -> list[ActionGopher]:
+    """Renvoie la liste de tous les coups légaux pour un joueur donné"""
+    liste_coups = []
+    for ligne_coup in grille[0]:
+        for coup in ligne_coup: #parcours de tout les coup (qui sont des Cell = tuple(int, int) ou des None)
+            if coup != None and est_legal(grille, coup, joueur): #si le coup est légal
+                liste_coups.append(coup) #on l'ajoute à la liste des coups légaux
+    return liste_coups
+
+
+
+
+    
+    
 
 
 
