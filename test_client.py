ba6c2048d2c475fdd74746ae50d57fe780af3ef2
KolinMTG <colin.manyri@gmail.com>
2024-06-18 16:35:03 +0200

    debut de la fonction initialize rataché au serveur

--- test_client.py
+++ test_client.py
@@ -2,20 +2,49 @@
 
 import ast
 import argparse
+import dodo
+import gopher as goph #importation des elements du jeu 
 from typing import Dict, Any
 from gndclient import start, Action, Score, Player, State, Time, DODO_STR, GOPHER_STR
 
-Environment = Dict[str, Any]
+Environment = Dict[str, Any] #contients les elements necessaire a notre fonction min max. 
+
+#Grid = Union[GridGopher, GridDodo]
+#Environement = Dict[grille : Grid, dico_conversion : Dict_Conv, dico_legaux : ]
 
 
-def initialize(
-    game: str, state: State, player: Player, hex_size: int, total_time: Time
-) -> Environment:
-    print("Init")
-    print(
-        f"{game} playing {player} on a grid of size {hex_size}. Time remaining: {total_time}"
-    )
+
+def initialize(game: str, state: State, player: Player, hex_size: int, total_time: Time):
+    """initialise l'environement en fonction du jeu choisi"""
+
+    environement = {} #environement est un dictionnaire qui contient les elements necessaire à notre programme pour bien jouer au jeu.
+    if game == "dodo" or game == "Dodo" or game == "DODO":
+        #initialisation de l'environement pour dodo
+        environement["grille"], environement["dico_conversion"] = dodo.init_grille_dodo(hex_size-1)
+        environement["dico_legaux"] = dodo.init_legal_moves(environement["grille"], player)
+        environement["joueur"] = player
+        return environement
+
+    if game == "gopher" or game == "Gopher" or game == "GOPHER":
+        #initialisation de l'environement pour gopher
+        environement["grille"], environement["dico_conversion"] = goph.init_grille_gopher(hex_size-1)
+        environement["dico_legaux"] = goph.init_legal_moves(environement["grille"], player)
+        environement["joueur"] = player
+        return environement
+
+    print("Erreur: Le jeu n'existe pas")
     return {}
+
+
+
+# def initialize(
+#     game: str, state: State, player: Player, hex_size: int, total_time: Time
+# ) -> Environment:
+#     print("Init")
+#     print(
+#         f"{game} playing {player} on a grid of size {hex_size}. Time remaining: {total_time}"
+#     )
+#     return {}
 
 
 def strategy_brain(
@@ -25,13 +54,15 @@ def strategy_brain(
     print("Time remaining ", time_left)
     print("What's your play ? ", end="")
     s = input()
-    print()
-    t = ast.literal_eval(s)
-    return (env, t)
+    
+    t = ast.literal_eval(s) 
+    env = {"info": 1}
+    print(env, t)
+    return (env, t) #! renvoiyer ici l'environemenet et l'action a effectuer 
 
 
 def final_result(state: State, score: Score, player: Player):
-    print(f"Ending: {player} wins with a score of {score}")
+    print(f"Ending: {player} wins with a score of {score}") 
 
 
 if __name__ == "__main__":
